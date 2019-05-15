9c3d3c0f75b89159bfeef504eec4c0655d61fc62
Ken Partridge <kenp@wti.com>
2019-05-15 12:11:29 -0700

    Cleaned up names, added input to override default values, removed multiple run loop

--- power.py
+++ power.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import json
-import os, time
 import requests
 
 # supress Unverified HTTPS request, only do this is a verified environment
@@ -17,90 +16,98 @@ BASE_PATH = "/api/v2/config/power"
 USERNAME = "super"
 PASSWORD = "super"
 
-iCount = 0
-iFail  = 0
-while (iCount < 10000):
-	try:
-		print(URI+SITE_NAME+BASE_PATH)
-		headersJSON = {'content-type': 'application/json'}
-		r = requests.get(URI+SITE_NAME+BASE_PATH, auth=(USERNAME, PASSWORD), verify=False, headers=headersJSON)
+print("\n\nWTI Plug Power request program")
+print("----------------------------\n")
 
-		if (r.status_code == 200):
-#			os.system('clear')
-			parsed_json = r.json()
+tempdata = input("Enter URI [default: %s ]: " % (URI))
+if (len(tempdata) > 0):
+	URI = tempdata
 
-#			Uncomment to see the JSON return by the unit
-#			print parsed_json
+tempdata = input("Enter SITE_NAME [default: %s ]: " % (SITE_NAME))
+if (len(tempdata) > 0):
+    SITE_NAME = tempdata
 
-			cszStatusCode = parsed_json["status"]["code"][0]
-			if (int(cszStatusCode) != 0):
-				exit(1)
+tempdata = input("Enter USERNAME [default: %s ]: " % (USERNAME))
+if (len(tempdata) > 0):
+    USERNAME = tempdata
 
-			if (int(parsed_json['powerunit']) == 0):
-				print("NOT a Power Unit. No power Data is avaiable.")
-				exit(1)
+tempdata = input("Enter PASSWORD [default: %s ]: " % (PASSWORD))
+if (len(tempdata) > 0):
+    PASSWORD = tempdata
 
-			if (int(parsed_json['outletmetering']) == 0):
-				print("Outlet metering is not available on this unit.")
-				exit(1)
+try:
+    print("%s%s%s" % (URI, SITE_NAME, BASE_PATH))
+    headersJSON = {'content-type': 'application/json'}
+    r = requests.get(URI+SITE_NAME+BASE_PATH, auth=(USERNAME, PASSWORD), verify=False, headers=headersJSON)
+    if (r.status_code == 200):
+        parsed_json = r.json()
+        print(parsed_json)
 
-			# is the device is an Automaitc Transfer Switch type of power switch
-			iATS 		= int(parsed_json['ats'])
+        statuscode = parsed_json["status"]["code"][0]
+        if (int(statuscode) != 0):
+            exit(1)
 
-			# how many branches (input power plugs)
-			iBranchCount 	= int(parsed_json['branchcount'])
+        if (int(parsed_json['powerunit']) == 0):
+            print("NOT a Power Unit. No power Data is avaiable.")
+            exit(1)
 
-			# how many power outlets (output power plugs)
-			iPlugCount	= int(parsed_json['plugcount'])
+        if (int(parsed_json['outletmetering']) == 0):
+            print("Outlet metering is not available on this unit.")
+            exit(1)
 
-			# how many data power data sets where returned in the JSON
-			iDataPoints 	= int(parsed_json['powerdatacount'])
+        # is the device is an Automaitc Transfer Switch type of power switch
+        ats = int(parsed_json['ats'])
 
-			# how many plugs are assigned to each branch (inpout power plug)
-			iPlugsPerBranch = (iPlugCount / iBranchCount);
+        # how many branches (input power plugs)
+        branchcount = int(parsed_json['branchcount'])
 
-			iTempDataPoints = 0
+        # how many power outlets (output power plugs)
+        plugcount = int(parsed_json['plugcount'])
 
-			while (iTempDataPoints < iDataPoints):
-				iTempBanchCount = 0
+        # how many data power data sets where returned in the JSON
+        datapoints = int(parsed_json['powerdatacount'])
 
-				print(BASE_PATH)
+        # how many plugs are assigned to each branch (inpout power plug)
+        plugsperbranch = (plugcount / branchcount)
 
-				# print the timedate stamp that the follwoing power data was recorded
-				print("Data @ "+str(parsed_json['powerdata'][iTempDataPoints]["timestamp"]))
+        tempdatapoints = 0
 
-				while (iTempBanchCount < iBranchCount):
-					# start reading the Branch block
-					print("BRANCH "+str(iTempBanchCount+1)+"")
+        while (tempdatapoints < datapoints):
+            tempbranchcount = 0
 
-					cszBranchName = "branch"+str(iTempBanchCount+1)
+            print(BASE_PATH)
 
-					# Voltage of the branch being read
-					print("    Voltage:         "+parsed_json['powerdata'][iTempDataPoints][cszBranchName][0]['voltage1'] +" (volts)")
+            # print the timedate stamp that the follwoing power data was recorded
+            print("Data @ %s" % (parsed_json['powerdata'][tempdatapoints]["timestamp"]))
 
-					iTempPlugCountCount = 0
-					while (iTempPlugCountCount < iPlugsPerBranch):
-						# read the current of the plug being read
-						cszCurrentName = "current"+str(iTempPlugCountCount+1)
+            while (tempbranchcount < branchcount):
+                # start reading the Branch block
+                print("BRANCH %d" % (tempbranchcount+1))
 
-						print("    Plug #"+str(iTempPlugCountCount+1)+" Current: "+parsed_json['powerdata'][iTempDataPoints][cszBranchName][0][cszCurrentName]+"  (amps)")
+                branchname = ("branch%d" % (tempbranchcount+1))
 
-						# calulate the power (in watts) of the plug being read
-						iPower = (float(parsed_json['powerdata'][iTempDataPoints][cszBranchName][0][cszCurrentName]) * float(parsed_json['powerdata'][iTempDataPoints][cszBranchName][0]['voltage1']))
-						print("    Plug #"+str(iTempPlugCountCount+1)+" Power:   "+str(iPower)+"  (watts)")
+                # Voltage of the branch being read
+                print("    Voltage:         %s (volts)" % (parsed_json['powerdata'][tempdatapoints][branchname][0]['voltage1']))
 
-						iTempPlugCountCount = iTempPlugCountCount + 1
+                tempplugcount = 0
+                while (tempplugcount < plugsperbranch):
+                    # read the current of the plug being read
+                    currentname = ("current%d" % (tempplugcount+1))
 
-					iTempBanchCount = iTempBanchCount + 1
+                    print("    Plug #%d Current: %s  (amps)" % (tempplugcount+1, parsed_json['powerdata'][tempdatapoints][branchname][0][currentname]))
 
-				iTempDataPoints = iTempDataPoints + 1
-		else:
-			print(r.status_code)
+                    # calulate the power (in watts) of the plug being read
+                    iPower = (float(parsed_json['powerdata'][tempdatapoints][branchname][0][currentname]) * float(parsed_json['powerdata'][tempdatapoints][branchname][0]['voltage1']))
+                    print("    Plug #%d Power:   %.1f  (watts)" % (tempplugcount+1, iPower))
 
-	except requests.exceptions.RequestException as e:  # This is the correct syntax
-		print e
-		time.sleep(60)
-		iFail = iFail + 1
-	iCount = iCount + 1
-	print("iCount = "+str(iCount)+ ", iFail = "+str(iFail))
-	time.sleep(2)
+                    tempplugcount += 1
+
+                tempbranchcount += 1
+
+            tempdatapoints += 1
+    else:
+        print(r.status_code)
+        print(r.reason)
+
+except requests.exceptions.RequestException as e:  # This is the correct syntax
+    print (e)
